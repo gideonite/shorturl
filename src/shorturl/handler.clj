9fbd05a59567fb35f3aed776e941365de6877d75
Gideon Dresdner <gideond@gmail.com>
2013-11-01 22:41:42 -0400

    salty login

--- src/shorturl/handler.clj
+++ src/shorturl/handler.clj
@@ -20,13 +20,17 @@
     (fn [s] (hash (str s salt)))))
 
 (defn user?
-  "checks whether a user exists
+  "checks whether a username exists
   user -> boolean
   "
+  [name]
+  (not (nil? (@users name))))
+
+(defn password?
   [user]
-  (if-let [fetched-user (@users (:username user))]
-    (= (:password fetched-user) (:password user))
-    false))
+  (let [fetched-user (@users (:username user))
+        salted (hash-with-salt (:password user))]
+    (= salted (:password fetched-user))))
 
 (defn create-user
   [user]
@@ -34,32 +38,34 @@
                        (update-in user [:password] hash-with-salt))))
 
 (defroutes app-routes
-  (GET "/" [] (resp/file-response "resources/public/index.html"))
+  (GET "/" [] (resp/redirect "/login"))
 
   (GET "/login" [] (resp/file-response "resources/public/login.html"))
   (POST "/login" [username password]
-        (if (user? {:username username :password password})
-          (resp/file-response "resources/public/index.html")
+        (if (user? username)
+          (if (password? {:username username :password password})
+            (resp/file-response "resources/public/index.html")
+            (resp/response "invalid password"))
           (resp/response
             (str "username \"" username "\" not found, please sign up"))))
 
   (GET "/signup" [] (resp/file-response "resources/public/signup.html"))
   (POST "/signup" [username password]
         (let [user {:username username :password password}]
-          (if (user? user)
-            (resp/response "Sorry bud. You had hope that you remember your password.")
-            (create-user user)))
-        (resp/response (str  "Thank you " username " you are now signed up.")))
+          (if (user? username)
+            (resp/response "Sorry bud. You had better hope that you can figure out your password.")
+            (do
+              (create-user user)
+              (resp/response (str "Thank you " username " you are now signed up."))))))
 
   (GET "/user/:name" [name] (str name))
   (GET "/users" [] (str @users))
-
   (GET "/:id" [id] (let [id (read-string id)
                          record (nth @url-records id)]
                      (swap! url-records #(update-in % [id :count] inc))
                      (resp/redirect (:url (nth @url-records id)))))
   (POST "/" [url] (let [record (store-url-return-id url)]
-                    (str "<a href='" url "'>" (nth @url-records 0) "</a>" " " (:count record))))
+                    (str "<a href='" url "'>" (str record) "</a>" " ")))
   ;(route/resources "/")
   (route/not-found "Not Found"))
 
